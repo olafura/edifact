5a23312dce04a05bca643f7016ce7b53f4be9a35
Olafur Arason <olafura@olafura.com>
2025-07-08 16:33:27 +0200

    feat: support replacing service advice with defaults
    I'm doing this to ease parsing the lines so they are consistent

--- lib/edifact/parser.ex
+++ lib/edifact/parser.ex
@@ -6,19 +6,19 @@ defmodule Edifact.Parser do
 
   data_element_seperator =
     ascii_string([0..255], 1)
-    |> tag(:data_element_seperator)
+    |> unwrap_and_tag(:data_element_seperator)
 
   decimal_notation =
     ascii_string([?., ?,], 1)
-    |> tag(:decimal_notation)
+    |> unwrap_and_tag(:decimal_notation)
 
   release_indicator =
     ascii_string([0..255], 1)
-    |> tag(:release_indicator)
+    |> unwrap_and_tag(:release_indicator)
 
   segment_terminator =
     ascii_string([0..255], 1)
-    |> tag(:segment_terminator)
+    |> unwrap_and_tag(:segment_terminator)
 
   defparsec(
     :service_string_advice,
@@ -30,4 +30,46 @@ defmodule Edifact.Parser do
     |> ignore(string(" "))
     |> concat(segment_terminator)
   )
+
+  def parse_service_string_advice(first_line) do
+    with {:ok, parsed, "", _, _, _} <-
+           service_string_advice(first_line) do
+      {:ok, Map.new(parsed)}
+    else
+      _ -> nil
+    end
+  end
+
+  def apply_service_advice(
+        line,
+        %{
+          data_element_seperator: data_element_seperator,
+          decimal_notation: decimal_notation,
+          release_indicator: release_indicator,
+          segment_terminator: segment_terminator
+        }
+      ) do
+    release_indicator = if release_indicator == " ", do: "?", else: release_indicator
+
+    base_replacement_list = [data_element_seperator, decimal_notation, segment_terminator]
+    replacement_list = for(type <- base_replacement_list, do: "?#{type}") ++ base_replacement_list
+
+    line
+    |> String.replace(
+      ["+", ".", "'"],
+      fn
+        "+" when data_element_seperator !== "+" -> "?+"
+        "." when decimal_notation !== "." -> "?."
+        "'" when segment_terminator !== "'" -> "?'"
+        other -> other
+      end
+    )
+    |> String.replace(release_indicator, "?")
+    |> String.replace(replacement_list, fn
+      <<"?"::binary, _>> = escaped -> escaped
+      ^data_element_seperator -> "+"
+      ^decimal_notation -> "."
+      ^segment_terminator -> "'"
+    end)
+  end
 end
